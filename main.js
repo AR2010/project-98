2c38198c72458f80cc795a93700751255b1b3075
AR2010 <87716381+AR2010@users.noreply.github.com>
2022-09-24 17:51:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,4 +10,20 @@ recognition.onresult = function(event) {
     var content = event.results[0][0].transcript;
     console.log(content);
     document.getElementById("voiceoutput").innerHTML = content;
+    speak();
 }
+
+function speak() {
+    var synth = window.speechSynthesis;
+    say = document.getElementById("voiceoutput").value;
+    var utterthis = new SpeechSynthesisUtterance(say);
+    synth.speak(utterthis);
+    Webcam.attach(camera);
+}
+Webcam.set({
+    width: 400,
+    height: 300,
+    image_format: 'png',
+    png_quality: 100
+});
+var camera = document.getElementById("camera");
